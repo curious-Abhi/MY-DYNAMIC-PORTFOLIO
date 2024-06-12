c7ff97ab2eede70fbdee73e285386702b82c16ef
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-06-12 20:38:33 +0530

    files updated

--- DASHBOARD/src/App.jsx
+++ DASHBOARD/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import {} from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-   <>
-   
-   </>
-  )
-}
+    <Router>
+      <Route path="/" />
+      <Route path="/login" element={<Login />} />
+      <Route path="/password/forgot" element={<ForgotPassword />} />
+      <Route path="/password/reset/:token" element={<ResetPassword />} />
+      <Route path="/manage/skills" element={<ManageSkills />} />
+      <Route path="/manage/timeline" element={<ManageTimeline />} />
+      <Route path="/manage/projects" element={<ManageProjects />} />
+      <Route path="/view/project/:id" element={<ViewProject />} />
+      <Route path="/update/project/:id" element={<UpdateProject />} />
+    </Router>
+  );
+};
 
-export default App
+export default App;

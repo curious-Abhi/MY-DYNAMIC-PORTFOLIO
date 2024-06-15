0121c42199934e16902806e8632b51d905a0b355
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-06-15 12:23:34 +0530

    files updated

--- DASHBOARD/src/pages/HomePage.jsx
+++ DASHBOARD/src/pages/HomePage.jsx
@@ -1,26 +1,32 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 import { Button } from '@/components/ui/button'
 import { clearAllUsersErrors, logout } from '@/store/slices/userSlice'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { toast } from 'react-toastify'
 
 const HomePage = () => {
-  const dispatch=useDispatch();
-  const handleLogout=()=>{
-    dispatch(logout)
-  }
-  useEffect(()=>{
-    if(error){
-      toast.error(error);
-      dispatch(clearAllUsersErrors())
-    }
-    
-  })
-  return (
-   <>
-   <Button onClick={handleLogout}/>
-   </>
-  )
-}
+  const dispatch = useDispatch();
+  const { error, message } = useSelector((state) => state.user);
 
-export default HomePage
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllUsersErrors());
+    }
+    if (message) {
+      toast.success(message);
+    }
+  }, [error, message, dispatch]);
+
+  return (
+    <>
+      <Button onClick={handleLogout}>Logout</Button>
+    </>
+  );
+};
+
+export default HomePage;

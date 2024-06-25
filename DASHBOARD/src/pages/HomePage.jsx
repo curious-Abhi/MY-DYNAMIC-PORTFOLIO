59dc97b540754032f7f4500f97d0f4eaf0ee472a
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-06-25 08:56:00 +0530

    files updated

--- DASHBOARD/src/pages/HomePage.jsx
+++ DASHBOARD/src/pages/HomePage.jsx
@@ -1,29 +1,65 @@
-import React from 'react'
+import { clearAllUsersErrors, logout } from "@/store/slices/userSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import { Home, Package2 } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
 
 const HomePage = () => {
+  const { active, setActive } = useState("");
+  const { isAuthenticated, error, user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+    toast.success("Logged Out");
+  };
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllUsersErrors());
+    }
+    if (!isAuthenticated) {
+      navigateTo("/login");
+    }
+  }, [isAuthenticated]);
   return (
     <>
-    
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <aside className="fixed inset-y-0 left-0 hidden w-14 flex-col border-r bg-background sm:flex z-50">
+          <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+            <Link className="group flex h-p w-p shrink-0 items-center justify-center gap-2 rounded-full ">
+              <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    className={`flex h-9 w-9 items-center justify-center rounded-lg ${
+                      active === "Dashboard"
+                        ? "text-accent-foreground bg-accent"
+                        : "text-muted-foreground"
+                    }  transition-colors hover:text-foreground md:h-8 md:w-8`}
+                    onClick={() => setActive("Dashboard")}
+                  >
+                    <Home className="h-5 w-5" />
+                    <span className="sr-only">Dashboard</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Dashboard</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </nav>
+        </aside>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default HomePage;
 
 /*
 import React, { useEffect } from 'react'

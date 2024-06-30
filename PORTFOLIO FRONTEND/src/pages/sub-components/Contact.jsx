1f8326e94d3aec1d850604e80460aad4ce03ba4e
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-06-30 19:06:43 +0530

    files updated

--- PORTFOLIO FRONTEND/src/pages/sub-components/Contact.jsx
+++ PORTFOLIO FRONTEND/src/pages/sub-components/Contact.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-//import { Label } from "@/components/ui/label";
+import { Label } from "@/components/ui/label";
 import axios from "axios";
 import React, { useState } from "react";
 import { toast } from "react-toastify";

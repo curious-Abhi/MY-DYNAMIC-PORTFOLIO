8c07387328cc22ebc6c23df74a00e87a7937ce46
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-07-19 17:53:49 +0530

    files updated

--- BACKEND/routes/certficateRouter.js
+++ BACKEND/routes/certficateRouter.js
@@ -1,4 +1,5 @@
 import express from "express"
 import { addNewCertificate,deleteCertificate,getAllCertificates } from "../controllers/certificateController"
+import { isAuthenticated } from "../middlewares/auth";
 
 const router=express.Router;

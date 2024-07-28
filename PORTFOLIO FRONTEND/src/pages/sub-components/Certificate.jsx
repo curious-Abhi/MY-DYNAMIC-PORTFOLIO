0c799b91de740faa2a6d100f58cff9cb5cfa7355
ABHISHEK KUMAR <abhishekk67781@gmail.com>
2024-07-28 19:39:21 +0530

    files updated

--- PORTFOLIO FRONTEND/src/pages/sub-components/Certificate.jsx
+++ PORTFOLIO FRONTEND/src/pages/sub-components/Certificate.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
 
 const Certificate = () => {
   return (
